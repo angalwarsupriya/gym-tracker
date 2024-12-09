a887f9cb5b923547dbb3e231ddb710ce4be6af89
supriya <angalwarsupriya@gmail.com>
2024-12-09 19:27:01 +0530

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Home from './components/Home'
+
 import './App.css'
 import Header from './components/Header'
 import Features from './components/Features'
@@ -11,7 +12,8 @@ function App() {
   return (
     <>
    
-    <div className='home-bg-container'>
+    <div className='home-bg-container' >
+      <img src='images/woman-holding-weights-near-barbells.jpg' style={{height:'100%', width:'100%'}}/>
     </div>
     <div className='con'>
       <Header/>
